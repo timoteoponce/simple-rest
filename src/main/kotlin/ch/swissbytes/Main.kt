fe4c75d20b9747f2059732e705e3cdd9e4e2435c
Timoteo Ponce <timoteo.ponce@swissbytes.ch>
2017-10-13 12:05:21 -0400

    Step 2

--- src/main/kotlin/ch/swissbytes/Main.kt
+++ src/main/kotlin/ch/swissbytes/Main.kt
@@ -1,14 +1,33 @@
 package ch.swissbytes
 
-import spark.Spark.*
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import spark.Spark.get
+import spark.Spark.path
 
 
 object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        get("/hello") { req, res -> "Hello World" }
+        val usersRepo = UserRepository()
+        path("/users") {
+
+            get("") { req, res ->
+                jacksonObjectMapper().writeValueAsString(usersRepo.users)
+            }
+        }
     }
 
+}
+
+data class User(val name: String, val email: String, val id: Int)
+
+class UserRepository {
+    val users = hashMapOf(
+            0 to User(name = "Alice", email = "alice@alice.kt", id = 0),
+            1 to User(name = "Bob", email = "bob@bob.kt", id = 1),
+            2 to User(name = "Carol", email = "carol@carol.kt", id = 2),
+            3 to User(name = "Dave", email = "dave@dave.kt", id = 3)
+    )
 
 }
